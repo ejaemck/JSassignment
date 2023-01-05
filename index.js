88867fda8c4b086bcf9a21f7cbae8aa9bbc7bdcb
ejaemck <ejmckibbens@gmail.com>
2023-01-05 00:05:41 -0500

    Praticing JavaScript

---
+++ index.js
@@ -0,0 +1,16 @@
+let person = {
+    name: 'Ebony McKibbens',
+    height: "5'9",
+    country: 'United States'
+  };
+  
+  console.log(person)
+
+
+alert(person)
+
+function greet (name, height, country){
+    console.log(name + ' '  + height+ ' ' + country);
+}
+
+greet('Ebony', "5'9", 'United States');
